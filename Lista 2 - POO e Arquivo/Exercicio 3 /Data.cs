71fc6d91796c41f39df0a26f184c6e51100bf806
Jean Machado <79550947+jeanmachadocx@users.noreply.github.com>
2023-08-26 16:53:19 -0300

    Create Data.cs

---
+++ Lista 2 - POO e Arquivo/Exercicio 3 /Data.cs
@@ -0,0 +1,23 @@
+//Classe DATA
+using System;
+using System.Security.Cryptography;
+
+
+
+
+namespace Exercicio3
+{
+public class Data
+{
+public int dia { get; set; }
+public int mes { get; set; }
+
+
+public Data(int dia, int mes)
+{
+this.dia = dia;
+this.mes = mes;
+}
+}
+}
+
