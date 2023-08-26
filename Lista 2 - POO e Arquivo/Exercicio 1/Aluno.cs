61a5cd6e480bf90a5d5b492738581f4fe56e5b4e
Jean Machado <79550947+jeanmachadocx@users.noreply.github.com>
2023-08-26 16:30:21 -0300

    Resolução exercicio 1
    Main está no "Program"

---
+++ Lista 2 - POO e Arquivo/Exercicio 1/Aluno.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1
+{
+
+    public class Aluno
+    {
+
+        //atributos
+        private string nome;
+        private int idade;
+        private double peso;
+        private bool formando;
+        private char sexo;
+
+        public Aluno()
+        {
+        }
+
+
+        //construtor
+        public Aluno(string nome, int idade, double peso, char sexo)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.peso = peso;
+            this.sexo = sexo;
+        }
+
+
+        //getters e setters     
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public double Peso { get; set; }
+
+        public char Sexo { get; set; }
+
+        public bool Formado { get; set; }
+
+    }
+}
---
+++ Lista 2 - POO e Arquivo/Exercicio 1/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+/*No método Main:
+
+
+• Depois, pergunte novamente ao usuário os valores da idade, do peso e se o aluno é formando.
+
+Altere estes atributos com os parâmetros informados por meio dos métodos modificadores
+
+(Set).
+
+• Por fim, imprima novamente os valores dos atributos do objeto Aluno usando os métodos de
+
+acesso (Get) de Aluno */
+
+
+namespace Exercicio1
+
+{
+
+    public class Program
+
+    {
+
+        public static void Main(string[] args)
+
+        {
+
+            Aluno aluno1 = new Aluno();
+
+            Console.WriteLine("Informe o nome:");
+
+            string nome = Console.ReadLine();
+
+            aluno1.Nome = nome;
+
+
+            Console.WriteLine("Idade: ");
+
+            int idade = int.Parse(Console.ReadLine());
+
+            aluno1.Idade = idade;
+
+
+            Console.WriteLine("Peso: ");
+
+            double peso = double.Parse(Console.ReadLine());
+
+            aluno1.Peso = peso;
+
+
+
+            Console.WriteLine("Sexo M/F: ");
+
+            char sexo = char.Parse(Console.ReadLine());
+
+            aluno1.Sexo = sexo;
+
+
+            Console.WriteLine("Você é formado? true || false");
+
+            bool formado = bool.Parse(Console.ReadLine());
+
+            aluno1.Formado = formado;
+
+
+            Console.WriteLine();
+
+            Console.WriteLine(aluno1.Nome);
+
+            Console.WriteLine(aluno1.Idade);
+
+            Console.WriteLine(aluno1.Peso);
+
+            Console.WriteLine(aluno1.Sexo);
+
+            Console.WriteLine(aluno1.Formado);
+
+
+
+            //da idade, do peso e se o aluno é formando.
+
+            Console.WriteLine("Digite uma nova idade: ");
+
+            aluno1.Idade = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Digite seu novo peso: ");
+
+            aluno1.Peso = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Formou? ");
+
+            aluno1.Formado = bool.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(aluno1.Idade);
+
+            Console.WriteLine(aluno1.Peso);
+
+            Console.WriteLine(aluno1.Formado);
+
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
