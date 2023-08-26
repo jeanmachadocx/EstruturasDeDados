f75a4b5fff5fd14998e0a4bdd3fbc13d54140dd6
Jean Machado <79550947+jeanmachadocx@users.noreply.github.com>
2023-08-26 16:32:25 -0300

    Resolução Exercicio 2

---
+++ Lista 2 - POO e Arquivo/Exercicio 2/Cliente.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    public class Cliente
+    {
+       
+        private int codigo;
+        private string nome;
+        private bool eClienteEspecial;
+        private double limiteCredito;
+        public Cliente(int codigo, string nome)
+        {
+            this.codigo = codigo; 
+            this.nome = nome;
+            limiteCredito = 0; 
+            eClienteEspecial = false;
+        }
+
+        public string GetNome()
+        {
+            return nome;
+        }
+
+        public int GetCodigo()
+        {
+            return codigo;
+        }
+
+        public bool AtualizarLimite(double valor)
+        {
+            if(eClienteEspecial == true)
+            {
+                limiteCredito = valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        
+        public void AtualizarCategoria(bool valor) 
+        {
+            eClienteEspecial = valor;
+
+            if(valor == false)
+            {
+                limiteCredito = 0;
+            }
+        }
+    }
+}
---
+++ Lista 2 - POO e Arquivo/Exercicio 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Exercicio2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Cliente cliente1 = new Cliente(01, "Jean");
+            Cliente cliente2 = new Cliente(02, "Maria");
+            Cliente cliente3 = new Cliente(03, "Fulano");
+
+            Console.WriteLine("Cliente 1 - Nome: " + cliente1.GetNome() + " Código: " + cliente1.GetCodigo());
+            Console.WriteLine("Cliente 2 - Nome: " + cliente2.GetNome() + " Código: " + cliente2.GetCodigo());
+            Console.WriteLine("Cliente 3 - Nome: " + cliente3.GetNome() + " Código: " + cliente3.GetCodigo());
+
+            cliente1.AtualizarCategoria(true);
+            cliente2.AtualizarCategoria(false);
+            cliente3.AtualizarCategoria(true);
+
+            Console.WriteLine();
+            Console.WriteLine("Cliente 1 é Cliente Especial? " + cliente1.AtualizarLimite(50000));
+            Console.WriteLine("Cliente 2 é Cliente Especial? " + cliente2.AtualizarLimite(50000));
+            Console.WriteLine("Cliente 3 é Cliente Especial? " + cliente3.AtualizarLimite(50000));
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
