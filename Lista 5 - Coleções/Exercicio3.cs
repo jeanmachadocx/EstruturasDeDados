f38b71dcad8160bf5adb06a181a15476efd389b9
Jean Machado <79550947+jeanmachadocx@users.noreply.github.com>
2023-09-30 18:56:47 -0300

    Create Exercicio3.cs

---
+++ Lista 5 - Coleções/Exercicio3.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Insira a sequência: ");
+            string sequencia = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach(char i in sequencia)
+            {
+                if (stack.Count > 0) // verificar se a pilha está vazia 
+                {
+                    if (i == '(' || i == '[')
+                    {
+                        stack.Push(i);
+                    }
+                    else if(i == ')')
+                    {
+                        if(stack.Count > 0 && stack.Peek() == '(')
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            Console.WriteLine("A pilha NÃO está formatada");
+                        }
+                    }
+                    else if (i == ']')
+                    {
+                        if (stack.Count > 0 && stack.Peek() == '[')
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            Console.WriteLine("A pilha NÃO está formatada");
+                        }
+                    }
+                } 
+            }
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("A sequência está formatada");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
